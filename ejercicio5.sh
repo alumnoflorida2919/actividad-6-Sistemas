83ee292755e1b10fd04a88fb2d9e6d0dd4c9de4f
alumno <alumno@pc-alumno.(none)>
2020-03-23 18:46:14 +0100

    subo el ejercicio5 con la media

--- ejercicio5.sh
+++ ejercicio5.sh
@@ -1,9 +1,14 @@
 read -p "introduce un numero, pulsa 0 para salir: " numero
 let suma=0
+let cont=0
+let media=0
 while [ $numero -ne 0 ]; do
+    cont=$(($cont+1))
     suma=$(($suma+$numero))
     read -p "introduce un numero, pulsa 0 para salir: " numero
 done
+media=$(($suma/$cont))
 echo "la suma de las cantidades es: $suma"
+echo "la media de la suma es: $media"
     
     
