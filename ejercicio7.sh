749a5fcc1bf72f586e44c919074c2210bf1f1f96
alumno <alumno@pc-alumno.(none)>
2020-03-24 12:48:35 +0100

    subo ejercicio7

---
+++ ejercicio7.sh
@@ -0,0 +1,99 @@
+read -p "Escribe un dia del mes: " dia
+
+      
+  case $dia in
+     1)
+        echo "lunes"
+     ;;
+     2)
+        echo "martes"
+	;;
+     3)
+        echo "miercoles"
+     ;;
+     4)
+        echo "jueves"
+	;;
+     5)
+        echo "viernes"
+     ;;
+     6)
+        echo "sabado"
+	;;
+      7)
+        echo "domingo"
+     ;;
+     8)
+        echo "lunes"
+	;;
+      9)
+        echo "martes"
+     ;;
+     10)
+        echo "miercoles"
+	;;
+      11)
+        echo "jueves"
+     ;;
+    12)
+        echo "viernes"
+	;;
+      13)
+        echo "sabado"
+     ;;
+     14)
+        echo "domingo"
+	;;
+      15)
+        echo "lunes"
+     ;;
+     16)
+        echo "martes"
+	;;
+      17)
+        echo "miercoles"
+     ;;
+     18)
+        echo "jueves"
+	;;
+      19)
+        echo "sabado"
+     ;;
+     20)
+        echo "domingo"
+	;;
+     21)
+        echo "lunes"
+     ;;
+     22)
+        echo "martes"
+	;;
+      23)
+        echo "miercoles"
+     ;;
+     24)
+        echo "jueves"
+	;;
+      25)
+        echo "viernes"
+     ;;
+     26)
+        echo "sabado"
+	;;
+      27)
+        echo "domingo"
+     ;;
+     28)
+        echo "lunes"
+	;;
+      29)
+        echo "martes"
+     ;;
+     30)
+         echo "miercoles"
+	 break
+     ;;
+     *)
+        echo "no pertenece a ningun dia"
+     ;;
+  esac
